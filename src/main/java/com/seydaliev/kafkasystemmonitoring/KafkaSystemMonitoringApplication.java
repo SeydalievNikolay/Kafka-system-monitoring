48eaeda5ee9c177d929ce427f13280152a1d8b79
Seydalievnikolay <118976386+Seydalievnikolay@users.noreply.github.com>
2024-04-16 15:25:47 +0700

    init

---
+++ src/main/java/com/seydaliev/kafkasystemmonitoring/KafkaSystemMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.seydaliev.kafkasystemmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSystemMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSystemMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seydaliev/kafkasystemmonitoring/KafkaSystemMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seydaliev.kafkasystemmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSystemMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
